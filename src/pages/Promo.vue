fe7434e05d6c1616338b56421840c6ff979df9ee
chunkey711 <chunkey95@gmail.com>
2017-04-07 18:29:45 +0300

    promo items

--- src/pages/Promo.vue
+++ src/pages/Promo.vue
@@ -5,8 +5,6 @@
     .wrapper
       .title
         | Акции
-      .promo-item При покупке двух кальянов, 3 шота в подарок
-
       .text
         .item(v-for="(item, index) in promos", :key="index")
           .title
@@ -34,11 +32,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss" rel="stylesheet/scss">
+
 .wrapper {
   background: #fff;
   height: calc(100vh - 140px);
   margin-top: 20px;
   padding: 20px;
+  overflow-y: auto;
 }
 
 .title {
@@ -48,30 +48,44 @@ export default {
   padding-bottom: 20px;
 }
 
-.promo-item {
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  font-family: 'Roboto Slab';
-  color: #fff;
-  text-decoration: none;
-  margin-right: 3%;
-  margin-bottom: 1.2vw;
-  display: flex;
-  width: 100%;
-  min-height: 128px;
-  position: relative;
-  border-radius: 2px;
-  box-shadow: 0 0 0 200px rgba(0, 0, 0, 0.2) inset, 0 1px 0 0 #d7d8db, 0 0 0 1px #e3e4e8;
-  text-shadow: 1px 1px 2px rgba(0,0,0,0.5);
-  padding: 10px 10px;
+.item {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: flex-start;
+    font-family: 'Roboto Slab';
+    color: #fff;
+    background-images: url('../assets/promo-image.jpg');
+    text-decoration: none;
+    margin-right: 3%;
+    margin-bottom: 1.2vw;
+    display: flex;
+    width: 100%;
+    min-height: 128px;
+    position: relative;
+    border-radius: 2px;
+    box-shadow: 0 0 0 200px rgba(0, 0, 0, 0.2) inset, 0 1px 0 0 #d7d8db, 0 0 0 1px #e3e4e8;
+    text-shadow: 1px 1px 2px rgba(0,0,0,0.5);
+    padding: 10px 10px;
+
+      .title {
+        font-family: 'Roboto Slab';
+        font-size: 24px;
+        font-wight: 300;
+        padding: 0;
+      }
+
+      .description {
+        font-family: 'Roboto Slab';
+        font-size: 20px;
+      }
 }
+
+
 
 .text {
   text-align: left;
   font-size: 16px;
-  margin-top: 20px;
   max-height: calc(100vh - 185px);
   overflow-y: auto;
 }
